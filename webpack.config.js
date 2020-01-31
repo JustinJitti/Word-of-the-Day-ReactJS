3933971af947e9d81080ea5647d28404868ba715
JustinJitti <JustinJitti@gmail.com>
2020-01-30 23:07:41 -0800

    Added Base Webpack Code

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'src/dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+            }
+        ]
+    }
+    
+}
